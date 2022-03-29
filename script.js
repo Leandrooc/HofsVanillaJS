1eab9b9dd57866585adda0150f19f19f000bd253
Leandro de Oliveira <19leandro3quietmsl@gmail.com.br>
2022-03-29 01:46:02 -0300

    Organization

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Test variables;
+
+const arrayForTests = [ 5, 1, 3, 4, 52, 23, 15, 2];
+const ObjectForTests = [{ age: 2}, { age: 6 }, { age: 5}, { age: 1}, { age: 3}]
+
+const callback = (itemArray) => itemArray < 20
+const callback2 = (itemArray) => itemArray > 6;
+const callbackObj = (itemArray) => itemArray.age > 3;
+
+//
 
 // Filter
 
@@ -14,13 +24,6 @@ const filter = (array, callback) => {
 
 //
 
-const arrayForTests = [ 5, 1, 3, 4, 52, 23, 15, 2];
-const ObjectForTests = [{ age: 2}, { age: 6 }, { age: 5}, { age: 1}, { age: 3}]
-
-const callback = (itemArray) => itemArray < 20
-const callback2 = (itemArray) => itemArray > 6;
-const callbackObj = (itemArray) => itemArray.age > 3;
-
 filter(arrayForTests, callback); // RETURN [ 5, 1, 3, 4, 15, 2 ]
 filter(arrayForTests, callback2); // RETURN [ 52, 23, 15 ]
 filter(ObjectForTests, callbackObj); // RETURN [ { age: 6 }, { age: 5 } ]
@@ -35,7 +38,11 @@ const Find = (array, callback) => {
   }
 }
 
+//
+
 Find(ObjectForTests, callbackObj); // RETURN { age: 6 }
 Find(arrayForTests, callback2); // RETURN 52
 
 //
+
+// Some and Every;
